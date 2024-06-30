98aea241d2d06f7c129e82e3953d9b31b6b6f15e
wagner <wagncarv@outlook.com>
2024-06-30 18:52:05 -0300

    style: code formatting

--- test/currency_convertion_web/controllers/error_json_test.exs
+++ test/currency_convertion_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule CurrencyConvertionWeb.ErrorJSONTest do
   use CurrencyConvertionWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert CurrencyConvertionWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert CurrencyConvertionWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
