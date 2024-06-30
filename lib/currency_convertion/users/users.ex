237d0955b228a0ea03ade6629d29d5ed3d2d653f
wagner <wagncarv@outlook.com>
2024-06-30 19:58:41 -0300

    refactor:  add get_user function, refactor add_transaction

--- lib/currency_convertion/users/users.ex
+++ lib/currency_convertion/users/users.ex
@@ -8,7 +8,15 @@ defmodule CurrencyConvertion.Users.Users do
   alias CurrencyConvertion.Users.User
   alias Ecto.Changeset
 
-  @spec transactions(integer()) :: list()
+  @spec get_user(integer()) :: {:ok, User.t()} | {:error, :not_found}
+  def get_user(user_id) do
+    case Repo.get(User, user_id) do
+      nil -> {:error, :not_found}
+      user -> {:ok, user}
+    end
+  end
+
+  @spec transactions(integer()) :: list(Transaction.t())
   def transactions(user_id) do
     User
     |> where([u], u.id == ^user_id)
@@ -17,11 +25,11 @@ defmodule CurrencyConvertion.Users.Users do
     |> List.flatten()
   end
 
-  @spec add_transaction(integer(), User.t()):: {:ok, User.t()}
+  @spec add_transaction(integer(), Transaction.t()) :: {:ok, User.t()}
   def add_transaction(user_id, transaction) do
-    case Repo.get(User, user_id) do
-      nil -> {:error, :not_found}
-      user -> add(user, transaction)
+    case get_user(user_id) do
+      {:error, _} = error -> error
+      {:ok, user} -> add(user, transaction)
     end
   end
 
