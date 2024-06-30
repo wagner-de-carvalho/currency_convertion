8c0b19e48b9f58a997499ddf1565808eaf1a1957
wagner <wagncarv@outlook.com>
2024-06-30 18:46:54 -0300

    feat: add functions to add and query users transactions

---
+++ lib/currency_convertion/users/users.ex
@@ -0,0 +1,46 @@
+defmodule CurrencyConvertion.Users.Users do
+  @moduledoc """
+  Users module contains functions related to User struct
+  """
+  import Ecto.Query
+  alias CurrencyConvertion.Repo
+  alias CurrencyConvertion.Users.Transaction
+  alias CurrencyConvertion.Users.User
+  alias Ecto.Changeset
+
+  @spec transactions(integer()) :: list()
+  def transactions(user_id) do
+    User
+    |> where([u], u.id == ^user_id)
+    |> select([u], u.transactions)
+    |> Repo.all()
+    |> List.flatten()
+  end
+
+  @spec add_transaction(integer(), User.t()):: {:ok, User.t()}
+  def add_transaction(user_id, transaction) do
+    case Repo.get(User, user_id) do
+      nil -> {:error, :not_found}
+      user -> add(user, transaction)
+    end
+  end
+
+  defp add(user, transaction) do
+    transactions =
+      user.transactions ++
+        [
+          %Transaction{
+            origin_currency: transaction.from,
+            origin_amount: transaction.amount,
+            destiny_currency: transaction.to,
+            rate: transaction.rate,
+            amount: transaction.result,
+            user_id: user.id
+          }
+        ]
+
+    Changeset.change(user)
+    |> Changeset.put_embed(:transactions, transactions)
+    |> Repo.update()
+  end
+end
