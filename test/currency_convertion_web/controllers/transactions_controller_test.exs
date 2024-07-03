71831f858c60be12239ff624ebe74ac151f8fc9d
wagner <wagncarv@outlook.com>
2024-07-03 00:37:36 -0300

    test: add tests to transactions_controller

---
+++ test/currency_convertion_web/controllers/transactions_controller_test.exs
@@ -0,0 +1,65 @@
+defmodule CurrencyConvertionWeb.TransactionsControllerTest do
+  @moduledoc """
+  Tests for module transactions_controller
+  """
+  use CurrencyConvertionWeb.ConnCase
+  alias CurrencyConvertion.Factory
+  alias CurrencyConvertion.Users.Users
+
+  describe "list/2" do
+    test "lists all user's transactions", %{conn: conn} do
+      params = %{amount: 1, from: "USD", rate: 5.593377, result: 5.59337, to: "BRL"}
+      user = Factory.insert(:user)
+      Users.add_transaction(user.id, params)
+
+      %{"transactions" => transactions} =
+        conn
+        |> get(~p"/api/users/#{user.id}/transactions")
+        |> json_response(:ok)
+
+      assert Enum.count(transactions) == 1
+    end
+
+    test "when user does not exist it returns an error", %{conn: conn} do
+      response =
+        conn
+        |> get(~p"/api/users/999/transactions")
+        |> json_response(:not_found)
+
+      assert response == %{"error" => "not_found"}
+    end
+  end
+
+  describe "create/2" do
+    test "creates a transaction for a certain user", %{conn: conn} do
+      user = Factory.insert(:user)
+      params = %{amount: 2, from: "USD", to: "BRL", user_id: user.id}
+
+      response =
+        conn
+        |> post(~p"/api/users/transactions", params)
+        |> json_response(:created)
+
+      assert %{
+               "date_time" => _,
+               "destiny_currency" => "BRL",
+               "origin_amount" => 2,
+               "origin_currency" => "USD",
+               "rate" => _,
+               "transaction_id" => _,
+               "user_id" => _
+             } = response
+    end
+
+    test "when user does not exist it returns an error", %{conn: conn} do
+      params = %{amount: 2, from: "USD", to: "BRL", user_id: 999}
+
+      response =
+        conn
+        |> post(~p"/api/users/transactions", params)
+        |> json_response(:not_found)
+
+      assert response == %{"error" => "not_found"}
+    end
+  end
+end
